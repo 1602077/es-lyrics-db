4dfdd8ee4a9e06c21f6bbfc91cdb8f726809c62a
jcmunday <62025739+1602077@users.noreply.github.com>
2022-06-21 06:53:09 +0100

    feat: Metadata parsing

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -2,21 +2,37 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"strings"
 
+	jsonvalue "github.com/Andrew-M-C/go.jsonvalue"
 	fluentffmpeg "github.com/modfy/fluent-ffmpeg"
 )
 
-type AudioConfig struct {
-	OutputFormat string
-	SampleRate   int
-	NumChannels  int
-	BitRate      int
+type Metadata struct {
+	Artist   string `json:"artist"`
+	Album    string `json:"album"`
+	Title    string `json:"title"`
+	Track    string `json:"track"`
+	Duration string `json:"duration"`
+	Filename string `json:"filename"`
 }
 
-func ProcessAudioFile(in, out string, config AudioConfig) {
+type AudioConfig struct {
+	OutputFormat string `json:"outputFormat"`
+	SampleRate   int    `json:"sampleRate"`
+	NumChannels  int    `json:"numChannels"`
+	BitRate      int    `json:"bitRate"`
+}
+
+func ProcessAudio(in string, config AudioConfig) (*Metadata, error) {
 	buf := &bytes.Buffer{}
+
+	fn := in[strings.LastIndex(in, "/")+1:]
+	out := fmt.Sprintf("../data/%v.%v", fn, config.OutputFormat)
 
 	fluentffmpeg.NewCommand("").
 		InputPath(in).
@@ -31,6 +47,72 @@ func ProcessAudioFile(in, out string, config AudioConfig) {
 
 	logs, _ := ioutil.ReadAll(buf)
 	fmt.Println(string(logs))
+
+	m, err := ProbeMetadata(in)
+	if err != nil {
+		return &Metadata{}, err
+	}
+	return m, nil
+}
+
+func ProbeMetadata(in string) (*Metadata, error) {
+	d, err := fluentffmpeg.Probe(in)
+	if err != nil {
+		return &Metadata{}, err
+	}
+
+	// Marshal map[string]interface{} into []byte
+	dd, _ := json.Marshal(d)
+	if err != nil {
+		return &Metadata{}, err
+	}
+
+	j, err := jsonvalue.Unmarshal(dd)
+	if err != nil {
+		return &Metadata{}, err
+	}
+
+	// Parse Metadata from unmarshalled json
+	artist, err := j.GetString("format", "tags", "artist")
+	if err != nil {
+		log.Print(err)
+	}
+	album, err := j.GetString("format", "tags", "album")
+	if err != nil {
+		log.Print(err)
+	}
+	title, err := j.GetString("format", "tags", "title")
+	if err != nil {
+		log.Print(err)
+	}
+	track, err := j.GetString("format", "tags", "track")
+	if err != nil {
+		log.Print(err)
+	}
+	duration, err := j.GetString("format", "duration")
+	if err != nil {
+		log.Print(err)
+	}
+
+	return &Metadata{
+		Artist:   artist,
+		Album:    album,
+		Title:    title,
+		Track:    track,
+		Duration: duration,
+	}, nil
+}
+
+func BatchProccessAudio(in []string, config AudioConfig) {
+	// TODO (Jack, 21/06/2022): Switch to output chan of Metadata
+	done := make(chan struct{})
+	for _, s := range in {
+		go func(s string, config AudioConfig) {
+			ProcessAudio(s, config)
+			done <- struct{}{}
+		}(s, config)
+	}
+	<-done
 }
 
 func main() {
@@ -43,8 +125,15 @@ func main() {
 	}
 
 	fn := "01 Smoke Signals"
-	in := fmt.Sprintf("../testdata/%v.mp3", fn)
-	out := fmt.Sprintf("../testdata/output/%v.%v", fn, ac.OutputFormat)
+	in := fmt.Sprintf("../data/%v.mp3", fn)
 
-	ProcessAudioFile(in, out, ac)
+	BatchProccessAudio([]string{in}, ac)
+}
+
+func PrettyPrint(v map[string]interface{}) (err error) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err == nil {
+		fmt.Println(string(b))
+	}
+	return
 }
--- go/go.mod
+++ go/go.mod
@@ -4,6 +4,8 @@ go 1.18
 
 require (
 	cloud.google.com/go/speech v1.4.0
+	github.com/Andrew-M-C/go.jsonvalue v1.2.1
+	github.com/modfy/fluent-ffmpeg v0.1.0
 	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad
 )
 
@@ -15,7 +17,6 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
-	github.com/modfy/fluent-ffmpeg v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
